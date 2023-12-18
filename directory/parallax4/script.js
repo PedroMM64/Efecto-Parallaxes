fdd1852cd74d2ae96c64f02805587d542dc5c246
PedroMM64 <snupis3@live.com.mx>
2023-12-18 01:43:42 -0600

    Se corrige errores de efecto en el scrolling parallax

--- directory/parallax4/script.js
+++ directory/parallax4/script.js
@@ -1,13 +1,15 @@
 let dubaiText = document.getElementById('dubaiText');
 let burjkhalifa = document.getElementById('burjkhalifa');
 let stars = document.getElementById('stars');
+let building = document.getElementById('building')
 let button = document.getElementById('button');
 
 window.addEventListener('scroll', function() {
     let value = this.window.scrollY;
 
-    dubaiText.style.left = value * -2 + 'px';
-    burjkhalifa.style.left = value * 1 + 'px';
+    dubaiText.style.right = value * -2 + 'px';
+    burjkhalifa.style.right = value * 1 + 'px';
     stars.style.top = value * -2 + 'px';
+    building.style.right = value * -1 + 'px';
     button.style.marginRight = value * 3 +'px';
 });
